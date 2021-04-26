a1d1e6feb69806f71eb08c31f688d18c40e1ef5f
Timothy Sharkey <Techne3@gmail.com>
2021-04-25 18:56:43 -0600

    danInfo grid for cards

--- src/components/Dan.js
+++ src/components/Dan.js
@@ -1,17 +1,24 @@
 import React from "react";
 import data from "../data/dataDan";
+import "../styles/danInfo.scss";
 
 const Dan = () => {
   return (
-    <div>
+    <>
       <h1>Hello from dan</h1>
-      {data.products.map((x) => (
-        <>
-          <h1>{x.category}</h1>
-          <h1>{x.price}</h1>
-        </>
-      ))}
-    </div>
+      <div className="dan_wrapper">
+        {data.products.map((x) => (
+          <>
+            <div className="dan_info_container">
+              <div className="dan_info_card">
+                <h1>{x.category}</h1>
+                <h1>{x.price}</h1>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
   );
 };
 
